8bedbe156db8c4807fa512deaa530f53bfce0989
+ <+>
2023-11-24 22:22:44 -0600

    KT scope lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+var customerName = "bob"
+
+function upperCaseCustomerName(){
+   customerName = customerName.toUpperCase();
+   // this changes the customer name b/c we modified the global variable
+}
+
+
+ function setBestCustomer(){
+    bestCustomer = "not bob"
+    return bestCustomer;
+ }
+
+ function overwriteBestCustomer(){
+    bestCustomer = "maybe bob"
+    return bestCustomer;
+ }
+
+ const leastFavoriteCustomer = "dave"
+ 
+ function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "jeff"
+    return leastFavoriteCustomer;
+ }
+ // can't change the value because it was declared with const globally
